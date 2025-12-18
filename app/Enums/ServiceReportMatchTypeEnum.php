86886c3b3dd2721eacf6b59a4832a3f6166dd16f
Furkan Meclis <furkanmeclis@icloud.com>
2025-12-18 14:46:00 +0300

    feat: ServiceReportMatchTypeEnum eklendi

---
+++ app/Enums/ServiceReportMatchTypeEnum.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Enums;
+
+enum ServiceReportMatchTypeEnum: string
+{
+    case BEFORE = 'before';
+    case AFTER = 'after';
+
+    /**
+     * Get all labels as key-value array
+     *
+     * @return array<string, string>
+     */
+    public static function getLabels(): array
+    {
+        return [
+            self::BEFORE->value => 'Hizmet Öncesi',
+            self::AFTER->value => 'Hizmet Sonrası',
+        ];
+    }
+
+    /**
+     * Get the label for this enum case
+     *
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return self::getLabels()[$this->value] ?? $this->value;
+    }
+}
+
